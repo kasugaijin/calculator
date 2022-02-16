d220ab52149b48c4da854d687fa47059ee57b591
Ben <robinsonbena@gmail.com>
2022-02-15 17:12:06 -0800

    update, most functionality complete

--- script.js
+++ script.js
@@ -25,47 +25,111 @@ let object = {
 one.addEventListener('click', addOne);
 
 function addOne() {
-  if (!('operand' in object)) {
-    object["numOne"] = 1;
-    current.textContent = object.numOne;  
-  } else {
-      object["numTwo"] = 1;
-      current.textContent = object.numTwo;
-  }
-  
-}
+    if (('product' in object) && !('operand' in object)) {
+        return false;
+    } else if (!('operand' in object) && !('numOne' in object)) {
+    object["numOne"] = [1];
+    current.textContent = object.numOne;
+    } else if (!('operand' in object)) {
+        object["numOne"] += [1];
+        current.textContent = object.numOne;
+    }
+  if (!('numTwo' in object) && ('operand' in object)){
+        object["numTwo"] = [1];
+        current.textContent = object.numTwo;
+    } else if ('numTwo' in object){
+         object["numTwo"] += [1];
+         current.textContent = object.numTwo;
+    }
+  } 
 
 plus.addEventListener('click', addPlus);
 
 function addPlus() {
-    if(!('numOne' in object)) {
+    if (('product' in object) && ('operand' in object) && ('numTwo' in object)) {
+        operate(object.product, object.operand, object.numTwo);
+        current.textContent = object.product + "+";
+        object['operand'] = "+";
+    } else if (('numOne' in object) && ('operand' in object) && ('numTwo' in object)) {
+        operate(object.numOne, object.operand, object.numTwo);
+        current.textContent = object.product + "+";
+        object['operand'] = "+";
+    }
+    else if(!('numOne' in object) && !('product' in object)) {
         return false;
     } else {
-    object["operand"] = "+";
-    current.textContent = object.numOne + "+"
+    if('numOne' in object) {
+       object['operand'] = "+";
+       current.textContent = object.numOne + "+"; 
+    } else {
+        current.textContent = object.product + "+";
+        object['operand'] = "+";
+    } 
 }
 }
 
-equals.addEventListener('click', operate(object.numOne, object.operand, object.numTwo));
+point.addEventListener('click', addPoint)
 
+function addPoint() {
+    //nothing, start numOne with 0.
+    //there is numOne but nothing else, and if no . exists, += to NumOne
+    //there is operand but no numTwo, start numTwo with it
+    //there is numTwo, if no . exists, then += to numTwo
+    //product cannot be modified**
+}
 
+function deleteLast() {
+    //nothing, then return false
+    //there is numOne but nothing else, slice last value of numOne
+    //there numOne and operand, slice operand
+    //there is numTwo, slice last value of numTwo
+    //product cannot be modified**
+}
+
+clr.addEventListener('click', clearAll)
+
+function clearAll() {
+    delete object.numOne;
+    delete object.operand;
+    delete object.numTwo;
+    delete object.product;
+    current.textContent = "";
+}
+
+equals.addEventListener('click', selectEquals);
+
+function selectEquals () {
+    if (!('numTwo' in object)) {
+        return false;
+    } else if (!('product' in object)) {
+        operate(object.numOne, object.operand, object.numTwo);
+    } else {
+        operate(object.product, object.operand, object.numTwo);
+    }
+}
 
 //Core arithmetic functions
 //to each, make the product become object.numOne to for continuity
 function addition (a, b) {
-    current.textContent = a + b;
+  
+    let result = Number(a) + Number(b);
+    current.textContent = result;
+    object['product'] = result;
+    delete object.numOne;
+    delete object.operand;
+    delete object.numTwo;
 }
 
 function subtraction (a, b) {
-    current.textContent = a - b;
+    current.textContent = Number(a) - Number(b);
 }
 
 function multiplication (a, b) {
-    current.textContentn = a * b;
+    current.textContentn = Number(a) * Number(b);
 }
 
 function division (a, b) {
-    let result = a / b;
+    let result = Number(a) / Number(b);
     current.textContent = result.toFixed(2);
 }
 
